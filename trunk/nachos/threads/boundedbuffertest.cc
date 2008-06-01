2fdf37227bcef263fd8c6f1b327589b74ffb6f9b
georgeganea <georgeganea@9ab8c1e2-f84d-0410-8123-6d1a1d1d2890>
2008-06-01 21:02:48 +0000

    more comments

--- trunk/nachos/threads/boundedbuffertest.cc
+++ trunk/nachos/threads/boundedbuffertest.cc
@@ -9,19 +9,19 @@ extern void BoundedBuffer(int sz), PutBoundedBuffer(int item);
 extern void GetBoundedBuffer(int item);
 
 void
-BoundedBufferTest()
+BoundedBufferTest() // function to launch the Producer-Consumer problem
 {
     DEBUG('t', "Entering SimpleTest");
     BoundedBuffer(7);
     
     int i;
-    for (i=0; i < NPRODUCERS; i++ ){
+    for (i=0; i < NPRODUCERS; i++ ){ // create 70 producers
     	char* str = new char[10];
     	sprintf(str,"%d",i);
     	Thread *t = new Thread(str);
     	t->Fork(PutBoundedBuffer, i);
     }
-    for ( i=0 ; i< NCONSUMERS ; i++ ){
+    for ( i=0 ; i< NCONSUMERS ; i++ ){ // and 100 consumers
     	char* str = new char[10];
     	sprintf(str,"%d",i+NPRODUCERS);
     	Thread *t = new Thread(str);
